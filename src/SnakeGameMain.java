bb10be31b93c48fc9c6ad7e2f0dbb33e5058c67c
Nina Cunha Pinheiro <ninac.pinheiro@outlook.com>
2026-06-06 20:01:07 -0300

    Add main class for Snake Game with resource handling
    Implemented main game class with resource management.

--- src/SnakeGameMain.java
+++ src/SnakeGameMain.java
@@ -1,3 +1,36 @@
-public class SnakeGameMain {
-    
+import com.badlogic.gdx.Game;
+import com.badlogic.gdx.graphics.g2d.BitmapFont;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.graphics.glutils.ShapeRenderer;
+import screen.MenuScreen;
+
+/**
+ * Main entry point for the LibGDX game.
+ * Manages global resources like SpriteBatch, ShapeRenderer, and fonts.
+ */
+public class SnakeGameMain extends Game {
+    public SpriteBatch batch;
+    public ShapeRenderer shapeRenderer;
+    public BitmapFont font;
+
+    @Override
+    public void create() {
+        batch = new SpriteBatch();
+        shapeRenderer = new ShapeRenderer();
+        font = new BitmapFont(); 
+        
+        this.setScreen(new MenuScreen(this));
+    }
+
+    @Override
+    public void render() {
+        super.render(); 
+    }
+
+    @Override
+    public void dispose() {
+        batch.dispose();
+        shapeRenderer.dispose();
+        font.dispose();
+    }
 }
